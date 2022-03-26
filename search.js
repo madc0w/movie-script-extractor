62d03c78a926c315318d654515629386b21ebd70
mad <marc@balloonary.io>
2022-03-26 21:24:43 +0100

    .

--- search.js
+++ search.js
@@ -5,36 +5,62 @@ const outDir = __dirname + '/out/';
 const inDir = __dirname + '/out/scripts/';
 // const inDir = __dirname + '/out/test/';
 
+// const testFile = 'A Few Good Men.txt';
+
 process.stdin.on('data', async input => {
 	input = input.toString();
-	const words = input.split(/\s+/);
+	let words = input.split(/\s+/);
 	words.pop();
+	// words.push('000000000000');
 
-	for (const file of await fs.readdirSync(inDir)) {
-		const script = fs.readFileSync(inDir + file).toString();
-		const cumulatedWords = [];
-		let prevIndex;
-		let i = 0;
-		for (const word of words) {
-			cumulatedWords.push(word);
-			let cumulatedRegex = '';
-			for (const cWord of cumulatedWords) {
-				cumulatedRegex += `\\b\\s*${cWord}\\b\\s*`;
-			}
-			const regex = new RegExp(cumulatedRegex, 'i');
-			const index = script.search(regex);
-			if ((index < 0 && prevIndex >= 0) || i == words.length - 1) {
-				if (index == -1) {
-					cumulatedWords.pop();
+	do {
+		let foundWords = [];
+		let foundInFile, foundIndex, foundInScript;
+		for (const file of await fs.readdirSync(inDir)) {
+			// if (file != testFile) {
+			// 	continue;
+			// }
+			const script = fs.readFileSync(inDir + file).toString();
+			// console.log(script);
+			const cumulatedWords = [];
+			let prevIndex;
+			let i = 0;
+			for (const word of words) {
+				cumulatedWords.push(word);
+				let cumulatedRegex = '';
+				for (const cWord of cumulatedWords) {
+					cumulatedRegex += `\\b\\s*${cWord}\\b\\s*`;
 				}
-				console.log(`found "${cumulatedWords.join(' ')}" in ${file} at index ${prevIndex}`);
-				break;
+				const regex = new RegExp(cumulatedRegex, 'i');
+				const index = script.search(regex);
+				if ((index < 0 || i == words.length - 1) && prevIndex >= 0) {
+					if (index == -1) {
+						cumulatedWords.pop();
+					}
+					if (cumulatedWords.length > foundWords.length) {
+						foundWords = JSON.parse(JSON.stringify(cumulatedWords));
+						// if (foundWords.length == 3) {
+						// 	console.log(foundWords);
+						// }
+						foundInFile = file;
+						foundIndex = prevIndex;
+						foundInScript = script;
+					}
+					break;
+				}
+				prevIndex = index;
+				i++;
 			}
-			prevIndex = index;
-			i++;
+			// console.log(script);
 		}
-		// console.log(script);
-	}
+
+		console.log(
+			`found "${foundWords.join(' ')}" in ${foundInFile} at index ${foundIndex}/${foundInScript.length} (${((100 * foundIndex) / foundInScript.length).toFixed(1)}%)`
+		);
+		words = words.splice(foundWords.length);
+		// console.log(words);
+	} while (words.length > 1);
+
 	console.log('done.');
 	process.exit();
 });
